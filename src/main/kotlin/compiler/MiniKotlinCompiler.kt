51b177f411c28c242207ce9154a173ab362ec625
Cikser <ciricmmarkoo@gmail.com>
2026-03-12 18:55:22 +0100

    Feature: expressions, variable assignment, functions and function calls

--- src/main/kotlin/compiler/MiniKotlinCompiler.kt
+++ src/main/kotlin/compiler/MiniKotlinCompiler.kt
@@ -5,12 +5,14 @@ import MiniKotlinParser
 
 class MiniKotlinCompiler : MiniKotlinBaseVisitor<String>() {
 
+    private var tabs: Int = 1
+
     fun compile(program: MiniKotlinParser.ProgramContext, className: String = "MiniProgram"): String {
-        var functions: String = ""
+        var functions = ""
         for(func in program.functionDeclaration()){
             functions += compileFunction(func) + "\n\n"
         }
-        return "public class ${className} {\n\n$functions}"
+        return "public class $className {\n\n$functions}"
     }
 
     fun javaType(kotlinType: String) = when (kotlinType) {
@@ -25,43 +27,57 @@ class MiniKotlinCompiler : MiniKotlinBaseVisitor<String>() {
         val name = function.IDENTIFIER().text
         val type = javaType(function.type().text)
         val paramList = function.parameterList()?.parameter() ?: emptyList()
-        val param_str = paramList.joinToString(", ")
-            {"${javaType(it.type().text)}} ${it.IDENTIFIER().text}"}
-        val block = compileBlock(function.block());
+        val paramStr = paramList.joinToString(", ")
+            {"${javaType(it.type().text)} ${it.IDENTIFIER().text}"}
+        val block = compileBlock(function.block())
         return if (name == "main")
-            "\tpublic static void main (String[] args) $block"
+            "${"\t".repeat(tabs)}public static void main (String[] args) $block"
         else
-            "TODO"
+            "${"\t".repeat(tabs)}public static $type $name ($paramStr) $block"
     }
 
     fun compileBlock(block: MiniKotlinParser.BlockContext): String {
+        tabs++;
         val statements = block.statement()
-        var statements_str = "{\n"
+        var statementsStr = "{\n"
         for (statement in statements) {
-            statements_str += "\t\t" + compileStatement(statement) + "\n"
+            statementsStr += "\t".repeat(tabs) + compileStatement(statement) + "\n"
         }
-        return statements_str + "\t}"
+        tabs--;
+        return "$statementsStr\t}"
     }
 
     fun compileStatement(statement: MiniKotlinParser.StatementContext): String {
         return when {
-            statement.variableDeclaration() != null -> { compileVariableDeclaration(statement.variableDeclaration()) }
+            statement.variableDeclaration() != null -> compileVariableDeclaration(statement.variableDeclaration())
+            statement.variableAssignment() != null -> compileVariableAssignment(statement.variableAssignment())
+            statement.returnStatement() != null -> compileReturnStatement(statement.returnStatement())
             else -> "TODO"
         }
     }
 
-    fun compileVariableDeclaration(var_decl: MiniKotlinParser.VariableDeclarationContext): String {
-        val type = javaType(var_decl.type().text)
-        val name = var_decl.IDENTIFIER().text
-        val expression = compileExpression(var_decl.expression())
+    fun compileVariableDeclaration(varDecl: MiniKotlinParser.VariableDeclarationContext): String {
+        val type = javaType(varDecl.type().text)
+        val name = varDecl.IDENTIFIER().text
+        val expression = compileExpression(varDecl.expression())
         return "$type $name = $expression;"
     }
 
     fun compileExpression(expression: MiniKotlinParser.ExpressionContext): String {
         return when (expression) {
+            is MiniKotlinParser.FunctionCallExprContext -> compileFunctionCall(expression)
             is MiniKotlinParser.PrimaryExprContext -> compilePrimary(expression.primary())
-            else -> "TODO"
+            is MiniKotlinParser.NotExprContext -> "!" + compileExpression(expression.expression())
+            else -> compileBinExpression(expression)
         }
+    }
+
+    fun compileFunctionCall(expression: MiniKotlinParser.FunctionCallExprContext): String {
+        val name = expression.IDENTIFIER().text
+        val argList = expression.argumentList()?.expression() ?: emptyList()
+        val argStr = argList.joinToString(", ")
+            {"${javaType(compileExpression(it))}"}
+        return "$name($argStr)"
     }
 
     fun compilePrimary(primary: MiniKotlinParser.PrimaryContext): String {
@@ -70,8 +86,87 @@ class MiniKotlinCompiler : MiniKotlinBaseVisitor<String>() {
             is MiniKotlinParser.BoolLiteralContext -> primary.BOOLEAN_LITERAL().text
             is MiniKotlinParser.StringLiteralContext -> primary.STRING_LITERAL().text
             is MiniKotlinParser.IdentifierExprContext -> primary.IDENTIFIER().text
-            else -> "TODO"
+            is MiniKotlinParser.ParenExprContext -> "(" + compileExpression(primary.expression()) + ")"
+            else -> throw IllegalArgumentException("invalid primary expression")
         }
+    }
+
+    fun compileBinExpression(expression: MiniKotlinParser.ExpressionContext): String {
+        return when (expression) {
+            is MiniKotlinParser.MulDivExprContext -> {
+                when {
+                    expression.DIV() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),"/")
+                    expression.MULT() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),"*")
+                    expression.MOD() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),"%")
+                    else -> throw IllegalArgumentException("invalid bin expression")
+                }
+            }
+            is MiniKotlinParser.AddSubExprContext -> {
+                when {
+                    expression.PLUS() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),"+")
+                    expression.MINUS() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),"-")
+                    else -> throw IllegalArgumentException("invalid bin expression")
+                }
+            }
+            is MiniKotlinParser.ComparisonExprContext -> {
+                when {
+                    expression.GE() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),">=")
+                    expression.GT() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),">")
+                    expression.LT() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),"<")
+                    expression.LE() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),"<=")
+                    else -> throw IllegalArgumentException("invalid comparison expression")
+                }
+            }
+            is MiniKotlinParser.EqualityExprContext -> {
+                when {
+                    expression.EQ() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),"==")
+                    expression.NEQ() != null -> resolveBinExpression(expression.expression(0),
+                        expression.expression(1),"!=")
+                    else -> throw IllegalArgumentException("invalid equality expression")
+                }
+            }
+            is MiniKotlinParser.AndExprContext -> {
+                if (expression.AND() != null) resolveBinExpression(expression.expression(0),
+                    expression.expression(1),"&&")
+                else throw IllegalArgumentException("invalid bin expression")
+            }
+            is MiniKotlinParser.OrExprContext -> {
+                if (expression.OR() != null) resolveBinExpression(expression.expression(0),
+                    expression.expression(1),"||")
+                else throw IllegalArgumentException("invalid bin expression")
+            }
+            else -> throw IllegalArgumentException("invalid expression")
+        }
+    }
+
+    fun resolveBinExpression(left: MiniKotlinParser.ExpressionContext,
+                             right: MiniKotlinParser.ExpressionContext,
+                             op: String): String {
+        return compileExpression(left) + " " + op + " " + compileExpression(right)
+    }
+
+    fun compileVariableAssignment(varAssign: MiniKotlinParser.VariableAssignmentContext): String {
+        val name = varAssign.IDENTIFIER().text
+        val expression = compileExpression(varAssign.expression())
+        return "$name = $expression;"
+    }
+
+    fun compileReturnStatement(returnStatement: MiniKotlinParser.ReturnStatementContext): String {
+        var expression = ""
+        if (returnStatement.expression() != null) {
+            expression = compileExpression(returnStatement.expression())
+        }
+        return "return $expression;"
     }
 
 }
